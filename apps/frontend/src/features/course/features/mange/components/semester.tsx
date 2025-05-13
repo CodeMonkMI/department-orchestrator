678778534be720261012f766919089ac21aff56a
Monirul Islam <mmislam027@gmail.com>
2025-05-13 19:19:16 +0600

    course: semester details showed

--- apps/frontend/src/features/course/features/mange/components/semester.tsx
+++ apps/frontend/src/features/course/features/mange/components/semester.tsx
@@ -1,25 +1,36 @@
 "use client";
 import Card from "@/components/ui-elements/Card";
 import { Badge } from "@/components/ui/badge";
-import { Input } from "@/components/ui/input";
-import { semesterData } from "@/features/course/data/semester";
-import { Search } from "lucide-react";
+import { useSemesterQuery } from "@/lib/api/semesterApi";
 import Link from "next/link";
 
 const Semester = () => {
+  const { data: semesters, isLoading, isError, error } = useSemesterQuery();
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading....</h2>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h2>Loading....</h2>
+      </div>
+    );
+  }
   return (
     <div>
       <Card className="p-6">
         <div className="flex items-center justify-between mb-4">
           <h2 className="text-lg font-semibold">Semesters</h2>
-          <div className="relative max-w-xs">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-            <Input placeholder="Search courses..." className="pl-9" />
-          </div>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {semesterData.map((semester) => (
+          {semesters!.map((semester) => (
             <Link
               href={`/semester/semester-${semester.id}`}
               key={semester.id}
@@ -28,11 +39,9 @@ const Semester = () => {
               <div className="flex justify-between items-start">
                 <div>
                   <h3 className="font-semibold">{semester.name}</h3>
-                  <p className="text-sm text-muted-foreground mt-1">
-                    {semester.period}
-                  </p>
+                  <p className="text-sm text-muted-foreground mt-1">6 Month</p>
                 </div>
-                <Badge variant="secondary">{semester.courses} Courses</Badge>
+                <Badge variant="secondary">0 Courses</Badge>
               </div>
             </Link>
           ))}
